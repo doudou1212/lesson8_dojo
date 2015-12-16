ea41bd95f2a82838ef72aae40d81748479715efa
doudou1212 <404891954@qq.com>
2015-12-16 14:24:05 +0800

    complete the dojo

--- baseline/baseline/spec/score-to-grade-spec.js
+++ baseline/baseline/spec/score-to-grade-spec.js
@@ -1,83 +1,73 @@
 describe('score_to_grade_spec',function(){
-var score = [59,69,79,89,99];
+    var score = [59,69,79,89,99];
+    /* common*/
+    it("change score to be D", function() {
+        var result = scoreToGrade(30);
+        expect(result).toEqual('D');
+    });
+    it("change score to be C", function() {
+        var result = scoreToGrade(65);
+        expect(result).toEqual('C');
+    });
+    it("change score to be B", function() {
+        var result = scoreToGrade(75);
+        expect(result).toEqual('B');
+    });
+    it("change score to be A", function() {
+        var result = scoreToGrade(85);
+        expect(result).toEqual('A');
+    });
+    it("change score to be S", function() {
+        var result = scoreToGrade(95);
+        expect(result).toEqual('S');
+    });
 
+    /* top border*/
+    it("change score to be D", function() {
+        var result = scoreToGrade(59);
+        expect(result).toEqual('D');
+    });
+    it("change score to be C", function() {
+        var result = scoreToGrade(69);
+        expect(result).toEqual('C');
+    });
+    it("change score to be B", function() {
+        var result = scoreToGrade(79);
+        expect(result).toEqual('B');
+    });
+    it("change score to be A", function() {
+        var result = scoreToGrade(89);
+        expect(result).toEqual('A');
+    });
+    it("change score to be", function() {
+        var result = scoreToGrade(99);
+        expect(result).toEqual('S');
+    });
 
-/* common*/
-it("contains spec with an expectation of D", function() {
-  var result = scoreToGrade(30);
-  expect(result).toEqual('D');
-});
-it("contains spec with an expectation of C", function() {
-  var result = scoreToGrade(65);
-  expect(result).toEqual('C');
-});
-it("contains spec with an expectation of B", function() {
-  var result = scoreToGrade(75);
-  expect(result).toEqual('B');
-});
-it("contains spec with an expectation of A", function() {
-  var result = scoreToGrade(85);
-  expect(result).toEqual('A');
-});
- it("contains spec with an expectation of S", function() {
-  var result = scoreToGrade(95);
-  expect(result).toEqual('S');
-});
+    /* bottom border*/
+    it("change score to be D", function() {
+        var result = scoreToGrade(0);
+        expect(result).toEqual('D');
+    });
+    it("change score to be C", function() {
+        var result = scoreToGrade(60);
+        expect(result).toEqual('C');
+    });
+    it("change score to be B", function() {
+        var result = scoreToGrade(70);
+        expect(result).toEqual('B');
+    });
+    it("change score to be A", function() {
+        var result = scoreToGrade(80);
+        expect(result).toEqual('A');
+    });
+    it("change score to be S", function() {
+        var result = scoreToGrade(90);
+        expect(result).toEqual('S');
+    });
 
-/* top border*/
-it("contains spec with an expectation of D", function() {
-    var result = scoreToGrade(59);
-    expect(result).toEqual('D');
-  });
-it("contains spec with an expectation of C", function() {
-    var result = scoreToGrade(69);
-    expect(result).toEqual('C');
-  });
-it("contains spec with an expectation of B", function() {
-    var result = scoreToGrade(79);
-    expect(result).toEqual('B');
-  });
-it("contains spec with an expectation of A", function() {
-    var result = scoreToGrade(89);
-    expect(result).toEqual('A');
-  });
-it("contains spec with an expectation of S", function() {
-    var result = scoreToGrade(99);
-    expect(result).toEqual('S');
-  });
-  /* bottom border*/
-it("contains spec with an expectation of D", function() {
-    var result = scoreToGrade(0);
-    expect(result).toEqual('D');
-  });
-  it("contains spec with an expectation of C", function() {
-    var result = scoreToGrade(60);
-    expect(result).toEqual('C');
-  });
-it("contains spec with an expectation of B", function() {
-    var result = scoreToGrade(70);
-    expect(result).toEqual('B');
-  });
-  it("contains spec with an expectation of A", function() {
-    var result = scoreToGrade(80);
-    expect(result).toEqual('A');
-  });
-  it("contains spec with an expectation of S", function() {
-    var result = scoreToGrade(90);
-    expect(result).toEqual('S');
-  });
-  it("contains spec with an expectation of null", function() {
-    var result = scoreToGrade(100);
-    expect(result).toEqual(null);
-  });
-
-  it("contains spec with an expectation of null", function() {
-    var result = scoreToGrade(null);
-    expect(result).toEqual(null);
-  });
-
-  it("contains spec with an expectation of null", function() {
-    var result = scoreToGrade('A');
-    expect(result).toEqual(null);
-  });
+    /*wrong score */
+    it("wrong score", function() {
+        expect(function(){scoreToGrade(101);}).toThrow();
+    });
 });
--- baseline/baseline/src/score-to-grade.js
+++ baseline/baseline/src/score-to-grade.js
@@ -1,14 +1,14 @@
 function scoreToGrade(score){
     if(0 <=score && score <60)
-      return 'D';
+        return 'D';
     else if(60 <=score && score <70)
-      return 'C';
-      else if(70 <=score && score <80)
+        return 'C';
+    else if(70 <=score && score <80)
         return 'B';
-        else if(80 <=score && score <90)
-          return 'A';
-          else if(90 <=score && score <100)
-            return 'S';
-      else
-         return null;
+    else if(80 <=score && score <90)
+        return 'A';
+    else if(90 <=score && score <100)
+        return 'S';
+    else
+         throw new Error("wrong score");
 }
